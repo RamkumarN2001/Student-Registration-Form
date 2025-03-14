4e5bb26f75e7892c7a54dce67596879d74b799db
Ramkumar N <ramkumar.Nagarajan07@gmail.com>
2025-03-14 15:48:30 +0530

    Add files via upload
    Updated the form validation

--- student.js
+++ student.js
@@ -1,6 +1,13 @@
 var submit = document.getElementById("Done1");
 var table = document.getElementById("table");
 
+
+var NameE = document.getElementById("name")
+var ageE = document.getElementById("age")
+var genderE = document.getElementById("gender")
+var emailE = document.getElementById("email")
+var courseE = document.getElementById("course")
+
 submit.addEventListener("click", function (event) {
     event.preventDefault(); // Prevent form submission (if inside a form)
 
@@ -10,10 +17,64 @@ submit.addEventListener("click", function (event) {
     var email = document.getElementById("email").value.trim();
     var course = document.getElementById("course").value.trim();
 
+    let isValid = true;
+
     if (!name || !age || !gender || !email || !course) {
         alert("All fields are required!");
         return;
     }
+
+    var NNameRegex = /^[a-zA-Z]+$/
+    var AgeRegex = /^\d.$/
+    var genderRegex = /^[a-zA-Z]+$/
+    var courseRegex = /^[a-zA-Z]+$/
+    var emailRegex = /^[a-zA-Z0-9\.]*@gmail\.com$/
+
+    if (NNameRegex.test(NameE.value) == false) {
+        document.querySelector(".nameerror").style.display = "inline"
+        isValid = false;
+    }
+    else {
+        document.querySelector(".nameerror").style.display = "none"
+    }
+
+    if (AgeRegex.test(ageE.value) == false) {
+        document.querySelector(".ageerror").style.display = "inline"
+        isValid = false;
+    }
+    else {
+        document.querySelector(".ageerror").style.display = "none"
+    }
+    if (emailRegex.test(emailE.value) == false) {
+        document.querySelector(".emailerror").style.display = "inline"
+        isValid = false;
+    }
+    else {
+        document.querySelector(".emailerror").style.display = "none"
+    }
+
+    if (courseRegex.test(courseE.value) == false) {
+        document.querySelector(".courseerror").style.display = "inline"
+        isValid = false;
+    }
+    else {
+        document.querySelector(".courseerror").style.display = "none"
+    }
+
+
+    if (isValid) {
+        alert("Form submitted successfully!");
+        addrow();
+        return true;
+    }
+    else {
+        return false;
+    }
+
+
+
+    function addrow(){
+
 
     var tr = document.createElement("tr");
 
@@ -42,4 +103,7 @@ submit.addEventListener("click", function (event) {
 
     // Reset the form after submission
     document.getElementById("regForm").reset();
+
+}
+
 });
